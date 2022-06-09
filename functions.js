886f12d06eddd9aa94e342bd5f5301c748a5fc66
Adryan-Maikel <maikeladryan@gmail.com>
2022-06-09 09:10:58 -0300

    ajustando jogabilidade

--- functions.js
+++ functions.js
@@ -1,44 +1,48 @@
 var diryJ, dirxJ, jog, velJ, pjx, pjy;
 var tamTelaW, tamTelaH;
 var jogo;
-var frame;
+var frames;
 
 function teclaDw(){
-    var tecla=event.keyCode;
-    if(tecla==38){//cima
-        diryJ=-1;
-    }else if(tecla==40){//baixo
-        diryJ=1;
-    }
-    if(tecla==37){//esquerda
-        dirxJ=-1;
-    }else if(tecla==39){//direita
-        dirxJ=1;
-    }
-    if(tecla==32){//espaço
-
-    }
+	var tecla=event.keyCode;
+	if(tecla==38){//Cima
+		diryJ=-1;
+	}else if(tecla==40){//Baixo
+		diryJ=1;
+	}
+	if(tecla==37){//Esquerda
+		dirxJ=-1;
+	}else if(tecla==39){//Direita
+		dirxJ=1;
+	}
+	if(tecla==32){//Espaço / Tiro
+		//TIRO
+	}
 }
 function teclaUp(){
-    var tecla=event.keyCode;
-    if((tecla==38)||(tecla==40)){
-        diryJ=0;
-    }
-    if((tecla==37)||(tecla==39)){
-        dirxJ=0;
-    }
+	var tecla=event.keyCode;
+	if((tecla==38)||(tecla==40)){
+		diryJ=0;
+	}
+	if((tecla==37)||(tecla==39)){//Esquerda
+		dirxJ=0;
+	}
 }
 
 function controlaJogador(){
-    pjy+=
+    pjy+=diryJ*velJ;
+    pjx+=dirxJ*velJ;
+    jog.style.top=pjy+"px";
+    jog.style.left=pjx+"px";
+    
 }
 
 function gameLoop(){
     if(jogo){
         //funções de controle
-        
+        controlaJogador();
     }
-    frame=requestAnimationFrame(gameLoop);
+    frames=requestAnimationFrame(gameLoop);
 }
 
 function inicia(){
@@ -56,8 +60,10 @@ function inicia(){
     jog=document.getElementById("navJog");
     jog.style.top=pjy+"px";
     jog.style.left=pjx+"px";
+
+    gameLoop();
 }
 
-window.addEventListener("load",inicia);
-document.addEventListener("keydown",teclaDw);
-document.addEventListener("keydown",teclaUp);
+window.addEventListener("load", inicia);
+document.addEventListener("keydown", teclaDw);
+document.addEventListener("keyup", teclaUp);
